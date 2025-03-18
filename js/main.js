28f4b0bf4df3fcb35ea050a2eaf8573d70d4f71f
Shennawy <ahmedshennawy997@gmail.com>
2025-03-18 19:25:45 +0200

    all

---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn").addEventListener("click", fetchRandomAyah);
+});
+
+async function fetchRandomAyah() {
+  try {
+    let randomAyahNumber = Math.floor(Math.random() * 6236) + 1;
+    let response = await fetch(
+      `https://api.alquran.cloud/v1/ayah/${randomAyahNumber}/editions/quran-uthmani,en.sahih`
+    );
+    let data = await response.json();
+
+    if (data.data) {
+      let arabicAyah = data.data[0].text;
+      let englishTranslation = data.data[1].text;
+
+      document.getElementById("ayah").innerText = `" ${arabicAyah} "`;
+      document.getElementById(
+        "translatedAyah"
+      ).innerText = `" ${englishTranslation} "`;
+    } else {
+      console.error("Error getting data");
+    }
+  } catch (error) {
+    console.error("Error getting ayah", error);
+  }
+}
+
+fetchRandomAyah();
